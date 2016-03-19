4790da59b2d45029d792d7732274a01e2a18cdd7
Chris Smith <smitty1e@gmail.com>
2016-03-19 18:22:26 -0400

    Adding unit test from Flask book

---
+++ test/test_configuration.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+from   flask       import request
+from   ../app/main import app
+
+class AppTest(unittest.TestCase):
+    """Does the thing work?
+    
+    """
+    def setUp(self):
+        self.app = app.test_client()
+
+    def test_homepage_works(self):
+        response = sefl.app.get("/")
+        self.assertEqual(response.status_code, 200)
+        
+if __name__ == '__main__':
+    unittest.main()
+        
